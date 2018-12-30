235d0e0729148fad36f118082116a016a5a33425
Dwin <dwin.e357@gmail.com>
2018-12-30 12:11:58 -0500

    First commit

---
+++ src/main/java/io/github/evolutionThroughCraft/service/trivia/TriviaApplication.java
@@ -0,0 +1,14 @@
+package io.github.evolutionThroughCraft.service.trivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaApplication.class, args);
+	}
+
+}
+
